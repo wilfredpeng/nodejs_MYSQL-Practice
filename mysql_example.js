6980d350b124903507863862bb33419ab272881e
wilfredpeng <35424319+wilfredpeng@users.noreply.github.com>
2019-11-29 14:47:51 +0800

    Add files via upload

--- mysql_example.js
+++ mysql_example.js
@@ -3,15 +3,48 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "*******",
+    password: "Paofan@9783",
     database: "TestDB"
 });
 
-con.connect(function(err) {
-    if (err) {
-        console.log(err)
-        // console.log('connecting error');
-        return;
-    }
-    console.log('connecting success');
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    insertData()
 });
+
+function createDatabase(){
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+      });
+}
+function createTable() {
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+}
+
+function insertData() {
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+}
+
+function deleteData(){
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+}
+
+// 建立 con 連線物件，並利用 connet 進行連線
+// host：連線主機
+// user：帳號
+// password：密碼
+// database：資料庫
